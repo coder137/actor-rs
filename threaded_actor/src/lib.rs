9135e230264825b2fdc6a46e0ac239351bd8b6f8
Niket Naidu <coder137@gmail.com>
2023-11-27 19:30:35 -0800

    Update lib.rs

--- threaded_actor/src/lib.rs
+++ threaded_actor/src/lib.rs
@@ -151,36 +151,6 @@ mod tests {
     }
 
     #[test]
-    fn test_ping_poll() {
-        let actor = Actor::new(2, Ping { delay: None });
-        let actor_ref = actor.get_user_actor_ref();
-
-        let prev: Instant = Instant::now();
-        let mut promise = actor_ref.as_poll(());
-        loop {
-            let res = promise.poll_once();
-            if matches!(res.unwrap(), ActorRefPollInfo::Complete) {
-                break;
-            }
-        }
-        let current = Instant::now();
-
-        println!(
-            "Current: {:?} Prev: {:?} Diff: {:?}, Elapsed: {:?}",
-            current,
-            prev,
-            current.duration_since(prev),
-            prev.elapsed()
-        );
-
-        let res = actor
-            .get_command_actor_ref()
-            .block(ActorCommandReq::Shutdown);
-        assert!(matches!(res.unwrap(), ActorCommandRes::Shutdown));
-        assert!(actor.handle.join().unwrap().is_ok());
-    }
-
-    #[test]
     fn test_actor_multiple_messages() {
         let actor = Actor::new(1, Ping { delay: None });
         let actor_ref = actor.get_user_actor_ref();
@@ -191,37 +161,6 @@ mod tests {
         let _pong1 = actor_ref1.block(());
         let _pong2 = actor_ref2.block(());
 
-        let res = actor
-            .get_command_actor_ref()
-            .block(ActorCommandReq::Shutdown);
-        assert!(matches!(res.unwrap(), ActorCommandRes::Shutdown));
-        assert!(actor.handle.join().unwrap().is_ok());
-    }
-
-    #[test]
-    fn test_actor_queue_full() {
-        let actor = Actor::new(
-            1,
-            Ping {
-                delay: Some(Duration::from_secs(5)),
-            },
-        );
-        let actor_ref1 = actor.get_user_actor_ref();
-        let actor_ref2 = actor.get_user_actor_ref();
-
-        // Sends in queue
-        let mut promise1 = actor_ref1.as_poll(());
-        let res1 = promise1.poll_once();
-        assert!(res1.is_ok());
-        assert!(matches!(res1.unwrap(), ActorRefPollInfo::RequestSent));
-
-        // Queue is full
-        let mut promise2 = actor_ref2.as_poll(());
-        let res2 = promise2.poll_once();
-        assert!(res2.is_ok());
-        assert!(matches!(res2.unwrap(), ActorRefPollInfo::RequestQueueFull));
-
-        // Shutdown
         let res = actor
             .get_command_actor_ref()
             .block(ActorCommandReq::Shutdown);
@@ -246,23 +185,6 @@ mod tests {
     }
 
     #[test]
-    fn test_actor_send_after_shutdown_with_poll() {
-        let actor = Actor::new(1, Ping { delay: None });
-        let actor_ref = actor.get_user_actor_ref();
-
-        let res = actor
-            .get_command_actor_ref()
-            .block(ActorCommandReq::Shutdown);
-        assert!(matches!(res.unwrap(), ActorCommandRes::Shutdown));
-        assert!(actor.handle.join().unwrap().is_ok());
-
-        let mut promise = actor_ref.as_poll(());
-        let result = promise.poll_once();
-        assert!(result.is_err());
-        assert!(matches!(result.err().unwrap(), ActorError::ActorShutdown));
-    }
-
-    #[test]
     fn test_actor_bad_behavior_with_blocking() {
         let actor = Actor::new(1, SimulateThreadCrash);
         let actor_ref = actor.get_user_actor_ref();
@@ -270,23 +192,6 @@ mod tests {
         assert!(res.is_err());
         assert!(matches!(res.err().unwrap(), ActorError::ActorInternalError));
 
-        let result = actor.handle.join();
-        assert!(result.is_err());
-    }
-
-    #[test]
-    fn test_actor_bad_behavior_with_poll() {
-        let actor = Actor::new(1, SimulateThreadCrash);
-        let actor_ref = actor.get_user_actor_ref();
-
-        let mut promise = actor_ref.as_poll(());
-        loop {
-            let result = promise.poll_once();
-            if let Err(e) = result {
-                assert!(matches!(e, ActorError::ActorInternalError));
-                break;
-            }
-        }
         let result = actor.handle.join();
         assert!(result.is_err());
     }
